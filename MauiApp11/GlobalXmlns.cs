313ebe5be783ddf94eeb5e2418a27078a4f28207
MakumbaKrolWioski123 <ut_4010@zsp3zamosc.pl>
2025-12-11 14:16:33 +0100

    Zadania Maui 15 od 1 do 3

---
+++ MauiApp11/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiApp11")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiApp11.Pages")]
