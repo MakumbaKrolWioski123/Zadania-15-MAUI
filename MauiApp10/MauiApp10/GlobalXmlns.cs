1f71b02fe61332da2fb211209ee1df33e1392a44
MakumbaKrolWioski123 <ut_4010@zsp3zamosc.pl>
2025-12-11 14:15:31 +0100

    Delete MauiApp10 directory

--- MauiApp10/MauiApp10/GlobalXmlns.cs
+++
@@ -1,2 +0,0 @@
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiApp10")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiApp10.Pages")]
